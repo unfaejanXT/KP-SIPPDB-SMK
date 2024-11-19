6e88fc4be957d4bd76a87f8d08a3759c6b54da69
Janwar Ksatria Pamungkas <janwarksatriapamungkas@hotmail.com>
2024-11-20 03:17:51 +0700

    Perubahan Migration & Seeder

--- database/migrations/2024_07_02_053814_create_operator_table.php
+++ database/migrations/2024_07_02_053814_create_operator_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,14 +12,20 @@ return new class extends Migration
     {
         Schema::create('operator', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->string('nip')->unique();
-            $table->string('nama_operator')->nullable(); // disesuaikan dengan model
-            $table->string('jabatan');
-            $table->string('noHP')->nullable();
+            $table->string('nama_operator')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
         });
     }
-    
+
 
     /**
      * Reverse the migrations.
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\PeriodePPDB;
 use App\Models\Role;
-use App\Models\roles;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
